36fe0781847fa6aea7d2ba5d0b7c6839ab4bccbd
Sibonelo NKOSI <sinkosi@c4r8s1.wethinkcode.co.za>
2019-08-02 17:18:14 +0200

    Last update

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 /*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 11:17:44 by sinkosi           #+#    #+#             */
-/*   Updated: 2019/07/29 15:03:10 by sinkosi          ###   ########.fr       */
+/*   Updated: 2019/07/28 14:13:14 by sinkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "./includes/ft_printf.h"
 #include "./libft/includes/libft.h"
-/*
+
 int	main(int argc, char **argv)
 {
 	char *str;
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 		return (0);
 	filename = argv[0];
 	str = "These are words";
-printf("Le fichier{CYAN}%s{eoc} contien : {RED}%s{eoc}", filename, str);
+/*printf("Le fichier{CYAN}%s{eoc} contien : {RED}%s{eoc}", filename, str);*/
 	ft_printf(RED"THIS IS A TEST");
 	ft_printf(RESET);
 	ft_printf("CAN I UNDERLINE THIS");
@@ -34,44 +34,3 @@ printf("Le fichier{CYAN}%s{eoc} contien : {RED}%s{eoc}", filename, str);
 	ft_printf("%s\n", filename);
 	return (0);
 }
-*/
-
-#include <errno.h>
-
-int main()
-{
-  int my_integer = -42;
-  unsigned int my_ui = 23;
-  float my_float = 3.56;
-  double my_double = 424242.171717;
-  char my_char = 'w';
-  char my_string[] = "Pardon me, may I borrow your nose?";
-
-  printf ("Integer: %d\n", my_integer);
-  printf ("Unsigned integer: %u\n", my_ui);
-
-  printf ("The same, as hexadecimal: %#x %#x\n", my_integer, my_ui);
-
-  printf ("Floating-point: %f\n", my_float);
-  printf ("Double, exponential notation: %17.11e\n", my_double);
-
-  printf ("Single character: %c\n", my_char);
-  printf ("String: %s\n", my_string);
-
-  errno = EACCES;
-  printf ("errno string (EACCES): %m\n");
-
-  return 0;
-}
-/*
-The code example above produces the following output on a GNU system: 
-
-Integer: -42
-Unsigned integer: 23
-The same, as hexadecimal: 0xffffffd6 0x17
-Floating-point: 3.560000
-Double, exponential notation: 4.24242171717e+05
-Single character: w
-String: Pardon me, may I borrow your nose?
-errno string (EACCES): Permission denied
-*/
