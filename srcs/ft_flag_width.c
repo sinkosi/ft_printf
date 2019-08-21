d406caa9c35811ef1b4da68e7aec4ce1f37e9ffb
Sibonelo NKOSI <sinkosi@c4r8s1.wethinkcode.co.za>
2019-08-21 12:42:13 +0200

    printing 0s and spaces now

--- srcs/ft_flag_width.c
+++ srcs/ft_flag_width.c
@@ -14,13 +14,27 @@
 
 void	ft_flag_width(t_printf *my_printf)
 {
+	//printf("\nft_flag_width->flag_minus = %d\n", my_printf->flag_minus);
+	//printf("\nft_flag_width->field_width = %d\n", my_printf->field_width);
+	//printf("\nft_flag_width->flag_zero = %d\n", my_printf->flag_zero);
 	if (my_printf->flag_minus == 1)
+	{
+		/*while (my_printf->field_width > 0)
+		{
+			ft_putchar(' ');
+			my_printf->f_return += 1;
+			my_printf->field_width -= 1;
+		}
+		//printf("ft_flag_width.c - FIRST IF STATEMENT\n");*/
 		my_printf->flag_minus = 2;
+	}
 	else if (my_printf->flag_minus == 0 || my_printf->flag_minus == 2)
 	{
+		//printf("ft_flag_width -> else if statement my_printf->flag_zero = %d", my_printf->flag_zero);
 		if (my_printf->flag_zero == 1 && my_printf->precision == 0
 				&& my_printf->flag_minus == 0)
 		{
+		//	printf("ft_flag_width.c - IF Statement\n");
 			ft_flag_plus(my_printf);
 			while (my_printf->field_width > 0)
 			{
@@ -30,6 +44,7 @@ void	ft_flag_width(t_printf *my_printf)
 			}
 		}
 		else
+			//printf("ft_flag_width.c - ELSE LOOP\n");
 			while (my_printf->field_width > 0)
 			{
 				ft_putchar(' ');
