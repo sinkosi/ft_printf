94adcc796694dc288132c88e669c99be7b471421
Sibonelo NKOSI <sinkosi@c5r6s10.wethinkcode.co.za>
2019-08-29 11:47:31 +0200

    Test modifications

--- float.c
+++ float.c
@@ -6,7 +6,7 @@
 /*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 11:28:02 by sinkosi           #+#    #+#             */
-/*   Updated: 2019/08/29 11:31:03 by sinkosi          ###   ########.fr       */
+/*   Updated: 2019/08/29 11:42:42 by sinkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ int	main(void)
 {
 	float	n = 3.14;
 
-	int		i = 1;
+	int		i = n;
 	int		ans1 = n - i;
 	float	ans2 = n - i;
 
 	printf("Answer 1: Int : %d\n", ans1);
 	printf("Answer 2: Float: %f\n", ans2);
+
+	printf("The value of n is : %f\n", n);
+	printf("The value of i is : %d\n", i);
 
 	return (0);
 }
