2737d46432b012db80de2bdfcca4e799c504003d
Sibonelo NKOSI <sinkosi@c4r8s1.wethinkcode.co.za>
2019-08-30 15:07:47 +0200

    print LONG FLOAT

---
+++ srcs/ft_print_lf_float.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_lf_float.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/30 14:51:38 by sinkosi           #+#    #+#             */
+/*   Updated: 2019/08/30 15:06:01 by sinkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+static void	ft_size_significand(t_printf *my_printf, char *str)
+{
+	ft_flag_size(my_printf, ft_strlen(str));
+	if (my_printf->flag_minus == 0 && my_printf->flag_zero == 0
+		&& my_printf->flag_hash == 1)
+		ft_flag_width(my_printf);
+}
+
+static void	ft_size_mantissa(t_printf *my_printf, char *str)
+{
+	ft_flag_size(my_printf, ft_strlen(str));
+	if (my_printf->flag_minus == 0 && my_printf->flag_zero == 0
+		&& my_printf->flag_hash == 1)
+		ft_flag_width(my_printf);
+}
+
+void		ft_print_lf_float(t_printf *my_printf, long double n)
+{
+	char		*str;
+	char		*mantissa;
+	intmax_t	a;
+
+	a = n;
+	str = ft_ltoa_base(n, 10);
+	n = ft_fabs(n) - ft_fabs(a);
+	if (my_printf->prec_width <= 0)
+		my_printf->prec_width = 6;
+	a = ft_power_ten(ft_fabs(n), my_printf->prec_width);
+	mantissa = ft_ltoa_base(a, 10);
+	ft_flag_size(my_printf, ft_strlen(str));
+	ft_size_significand(my_printf, str);
+	ft_size_mantissa(my_printf, mantissa);
+	ft_flag_width(my_printf);
+	ft_putstr(str);
+	my_printf->f_return += ft_strlen(str);
+	ft_putstr(".");
+	my_printf->f_return += 1;
+	ft_putstr(mantissa);
+	my_printf->f_return += ft_strlen(mantissa);
+	if (my_printf->field_width > 0)
+		ft_flag_width(my_printf);
+	ft_strdel(&str);
+}
